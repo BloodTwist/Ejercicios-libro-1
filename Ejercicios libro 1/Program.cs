346090c648c72f2c69490064f740c5f2c029f8b6
Daniel <dsestupinan71@misena.edu.co>
2019-05-18 09:00:26 -0500

    Libro 1
    Ejercicio 1 pag. 74

--- Ejercicios libro 1/Program.cs
+++ Ejercicios libro 1/Program.cs
@@ -14,20 +14,24 @@ namespace Ejercicios_libro_1
 
             c = a + b++;
 
-            /* En esta parte la unica  diferencia con respecto a la operación ant. es que tenemos a "b++", lo que nos
-             dice que el valor de la variable "b" va a cambiar en la siguiente linea, es decir en el resultado de "c", 
-             la variable "b" */ 
+            /* En esta parte la unica  diferencia con respecto a la operación ant. es el operador entre estas, y que tenemos a "b++", lo que nos
+             dice que el valor de la variable "b" va a cambiar en la siguiente linea, es decir que en el resultado de "c", 
+             la variable "b" va a tener el valor con el cual se inicializó*/ 
 
             e = ++a - b;
+
+            /*En esta operación "b" ya tomo el valor de 4, debido al anterior "b++", y en este caso también la "++a" le va a pasar lo mismo que le pasó a la "b++",
+             * anterior*/
+
             y = (float)a / b;
+            /*En este caso se esta casteando un resultado, en este caso el cast se hace para que la división entre dos enteros quede en coma flotante (Decimal)    */
+
             Console.WriteLine($" x= {x} c= {c} e= {e} y= {y} ");
+            /* En esta parte se hace la impresión de las variables que contienen resultados de operaciones, esto con la finalidad de entender mejor como funciona el codigo*/
+
             Console.ReadLine();
 
-            /*Las variables "a" y "b" se van operando y almacenando en variables del tipo float y entero 
-            
-             */
-
-
+            /*El readline lo agregamos para que el programa no se cierre, sino tenga que esperar un enter para poder continuar y cerrarse */
         }
     }
 }
