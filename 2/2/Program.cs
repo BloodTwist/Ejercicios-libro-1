1a758989735d16ad7b1e2ccdbd754e94f284d774
Daniel <dsestupinan71@misena.edu.co>
2019-05-18 09:25:55 -0500

    Libro 1
    74 ejercicio 2

---
+++ 2/2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine($"La \x0071 minúscula y la \x0051 mayúscula con Unicode");
+            Console.ReadKey();
+        }
+    }
+}
