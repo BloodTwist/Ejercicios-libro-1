377d08721a72defea3b16ba592f71fce2f0eba39
Daniel <dsestupinan71@misena.edu.co>
2019-05-18 12:20:23 -0500

    libro1
    Ejercicio 5 pag 98

--- 5/5/Program.cs
+++ 5/5/Program.cs
@@ -4,30 +4,37 @@ namespace _5
 {
     class Program
     {
-        const int limInferior = 50;
-        const int limSuperior = 273;
-        const int incremento = 2;
+        const int limInferior = -30;
+        const int limSuperior =100;
+        const int incremento = 6;
 
         public static float ObtenerFahrenheit(float gradosC)
         {
-            return 9F / 5F * gradosC + 32;
+            float res = 9f/5f *gradosC+ 32;
+            /*La explicación del porqué el "9/5" se tiene que usar la letra "f", se debe al identificador
+             este nos dice que tomará un dato de coma flotante, de este modo no se puede poner 1.8 (Solución de 9/5) 
+             porque estamos diciendo que el dato es del tipo double, lo que crea conflictos. Asi que, la mejor solución es
+             hacer un "cast"*/
+
+            return res;
         }
 
         static void Main(string[] args)
         {
-            int gradosCent = limInferior;
+            int  gradosCent = limInferior;
             float gradosFahr = 0;
 
             while (gradosCent <= limSuperior)
             {
                 gradosFahr = ObtenerFahrenheit(gradosCent);
 
-                System.Console.WriteLine("{0,8:d} C {1,8:f2} F", gradosCent , gradosFahr);
+               // Console.WriteLine($"{gradosCent} C {gradosFahr} F");
+                System.Console.WriteLine("{0,8:d} C {1,8:f} F", gradosCent , gradosFahr);
                 
 
                 gradosCent += incremento;
                 Console.ReadKey();
-                /*Al cambiar los datos constantes lo que se genera es que el bucle en este caso se ejecute mas veces, lo que genera un dato mayor y diferente del ejemplo propuesto*/
+                
             }
 
             
