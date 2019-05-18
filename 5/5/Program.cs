e6f0ab387a82e327208f8fb975dd0519531be5fc
Daniel <dsestupinan71@misena.edu.co>
2019-05-18 11:08:34 -0500

    Libro 1
    pag74 ejercicio 5

---
+++ 5/5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        const int limInferior = 50;
+        const int limSuperior = 273;
+        const int incremento = 2;
+
+        public static float ObtenerFahrenheit(float gradosC)
+        {
+            return 9F / 5F * gradosC + 32;
+        }
+
+        static void Main(string[] args)
+        {
+            int gradosCent = limInferior;
+            float gradosFahr = 0;
+
+            while (gradosCent <= limSuperior)
+            {
+                gradosFahr = ObtenerFahrenheit(gradosCent);
+
+                System.Console.WriteLine("{0,8:d} C {1,8:f2} F", gradosCent , gradosFahr);
+                
+
+                gradosCent += incremento;
+                Console.ReadKey();
+                /*Al cambiar los datos constantes lo que se genera es que el bucle en este caso se ejecute mas veces, lo que genera un dato mayor y diferente del ejemplo propuesto*/
+            }
+
+            
+
+
+        }
+    }
+}
