cddb23e81d0cde94365a01da782e4f720709110b
Daniel <dsestupinan71@misena.edu.co>
2019-05-19 18:27:03 -0500

    Libro 1
    Pag 159 ejercicio 1

---
+++ 13/13/Operacion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13
+{
+    class Operacion
+    {
+
+        int mod5, mod2, a, b, inicio, fin;
+        public void Condicion (int a, int b)
+        {
+
+            if (a < b)
+            {
+                inicio = a;
+                fin = b;
+                for (int i = inicio; i < fin; i++)
+                {
+                    mod5 = i % 5;
+                    mod2 = i % 2;
+                    if ((mod5 == 0) && (i != 0))
+                    {
+                        Console.WriteLine($"Núm {i}");
+                    }
+
+                }
+            }
+            else
+            {
+                inicio = b;
+                fin = a;
+                for (int i = inicio; i < fin; i++)
+                {
+                    mod5 = i % 5;
+                    if ((mod5 == 0) && (i != 0))
+                    {
+                        mod5 = i % 5;
+                        Console.WriteLine($"Núm {i}");
+
+                    }
+
+                }
+
+
+            }
+
+
+        }
+
+    }
+}
---
+++ 13/13/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _13
+{
+    class Program
+    {
+        public static int index;
+        static void Main(string[] args)
+        {
+            int mod5, mod2, a, b, inicio, fin;
+
+            for (index = 0; index < 1; index++)
+            {
+
+
+                Console.Write("Digite inicio: ");
+                a = int.Parse((Console.ReadLine()));
+
+                Console.Write("Digite fin: ");
+                b = int.Parse((Console.ReadLine()));
+
+                Operacion m = new Operacion();
+                Program g = new Program();
+
+                if ((a > -1) && (b > -1))
+                {
+                    m.Condicion(a, b);
+                }
+                else
+                {
+                    Console.WriteLine("Digite valores positivos n");
+                    index--;
+
+                }
+            }
+            Console.ReadKey();
+            
+            
+
+
+
+
+        }
+    }
+}
