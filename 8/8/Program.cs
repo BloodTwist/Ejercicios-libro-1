f4fd53410944cb65b30dc221ecc8c8c090280abd
Daniel <dsestupinan71@misena.edu.co>
2019-05-18 21:39:15 -0500

    libro 1
    Pag 124 ejercicio 2

---
+++ 8/8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, ¿Cómo te llamas?");
+            String nombre = Console.ReadLine();
+
+            Console.WriteLine("¿En qué año naciste?");
+            int fNa=int.Parse( Console.ReadLine());
+            int rta = 2030-fNa;
+
+            Console.WriteLine($"Hola {nombre} en el año 2030 tendrás {rta} años");
+            Console.ReadKey();
+        }
+    }
+}
