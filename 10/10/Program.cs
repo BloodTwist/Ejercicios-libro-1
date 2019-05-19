029e69a7caeadd62729a8beec83c1e594b4b109b
Daniel <dsestupinan71@misena.edu.co>
2019-05-18 22:03:24 -0500

    libro 1
    Pag 124 ejercicio 4

---
+++ 10/10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x, res = 0, a, b, c ;
+
+            Console.WriteLine("Digite valores de coeficientes para la expresión ax^5 - bx^3 +cx -7 ");
+
+            Console.WriteLine("Digite el valor de a");
+            a = double.Parse((Console.ReadLine()));
+
+            Console.WriteLine("Digite el valor de b");
+            b = double.Parse((Console.ReadLine()));
+
+            Console.WriteLine("Digite el valor de c");
+            c = double.Parse((Console.ReadLine()));
+
+            Console.WriteLine("Digite el valor de x");
+            x = double.Parse((Console.ReadLine()));
+
+            res = (a * Math.Pow(x, 5)) - (b * Math.Pow(x, 3)) + (c * x) - 7;
+
+            Console.WriteLine($"para x = {x}, {a}x ^ 5 - {b}x ^ 3 + {c}x - 7 = {res}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
