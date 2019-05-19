da364f8c8a7c380bef4de0085f03bc35cd482aa3
Daniel <dsestupinan71@misena.edu.co>
2019-05-18 20:45:09 -0500

    libro 1
    Uptade 6

--- 6/6/Program.cs
+++ 6/6/Program.cs
@@ -4,9 +4,32 @@ namespace _6
 {
     class Program
     {
+        const int limInferior = -30;
+        const int limSuperior = 100;
+        const int incremento = 6;
+
+        public static float ObtenerFahrenheit(float gradosC)
+        {
+            return 9f / 5f * gradosC + 32;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int gradosCent = limInferior;
+            float gradosFahr = 0;
+
+            while (gradosCent <= limSuperior)
+            {
+                gradosFahr = ObtenerFahrenheit(gradosCent);
+
+                Console.WriteLine($"{gradosCent} C {gradosFahr} F");
+                //System.Console.WriteLine("{0,8:d} C {1,8:f} F", gradosCent , gradosFahr);
+
+
+                gradosCent += incremento;
+                Console.ReadKey();
+
+            }
         }
     }
 }
