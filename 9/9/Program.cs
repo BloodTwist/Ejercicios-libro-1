a4c7c59fa5e9b899c48847d2120910da046d4517
Daniel <dsestupinan71@misena.edu.co>
2019-05-18 21:56:23 -0500

    libro 1
    pag 124 ejercicio 3

---
+++ 9/9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3x^5 - 5x^3 +2x -7
+            //Para x = valor, 3x^5 - 5x^3 +2x -7 = resultado 
+
+            double x, res=0;
+
+            Console.WriteLine("Digite el valor de x, con el que se evulará la expresión 3x^5 - 5x^3 +2x -7 ");
+            x=double.Parse( (Console.ReadLine()));
+            res = (3*Math.Pow(x,5)) - (5* Math.Pow(x, 3)) + (2*x) - 7;
+            Console.WriteLine($"para x = {x}, 3x ^ 5 - 5x ^ 3 + 2x - 7 = {res}");
+            Console.ReadKey();
+
+        }
+    }
+}
