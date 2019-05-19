7500a3185b46e9b4cef6227f3faec06dff68fb5b
Daniel <dsestupinan71@misena.edu.co>
2019-05-18 21:27:58 -0500

    libro1
    pag 124 ejercicio 1

---
+++ 7/7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double res=0, radio;
+            double pi = Math.PI;
+            Console.WriteLine("Digite el radio de la esfera: ");
+            radio = double.Parse(Console.ReadLine());
+
+            res = (4d / 3d) * pi * (Math.Pow(radio, 3));
+
+            
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
