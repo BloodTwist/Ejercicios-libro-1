3267e43f66969629f5526fe9e1055abc3ba9e682
Daniel <dsestupinan71@misena.edu.co>
2019-05-19 19:12:49 -0500

    libro 1
    Pag 159 ejercicio 2

---
+++ 14/14/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite hasta donde llegará la sumatoria: ");
+           int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite hasta donde iniciará la sumatoria: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite el primer coeficiente de la sumatoria: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite el término independiente de la sumatoria: ");
+            int y = int.Parse(Console.ReadLine());
+
+            if (b>a)
+            { 
+                double sum, acum = 0;
+
+                for (int i = a; i <= b; i++)
+                {
+                    sum = 1 / ((double)x + ((double)i * (double)y));
+                    acum = acum + sum;
+                }
+
+                Console.Write($"El resultado de la sumatoria es: {acum} ");
+            }
+            else
+            {
+                Console.WriteLine("El valor del indice superior (Fin) es menor al valor del indice inferior (Inicio)");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
